300ba696f48dd04f42e187372e5afe95766b9090
Jean <jean_carlosb14@hotmail.com>
2023-08-14 15:54:47 -0300

    Colocando o modal de cadastro no ADMIN e no COMUN

---
+++ public/js/script.js
@@ -0,0 +1,27 @@
+function irparaopagamento() {
+    total = document.getElementById("total").value
+
+    if (total =="" || total == 0) {
+        alert("Informações inválidas!")
+    } else {
+        alert("Avançando para pagamento!")
+    }
+}
+
+function selecionarIgreja(igreja) {
+    qtde = document.getElementById("qtde").value
+    document.getElementById("buttonPagamento").disabled = false;
+    if (igreja ==  "") {
+        valor = ""
+        document.getElementById("buttonPagamento").disabled = true;
+    } else if (igreja <= 1) {
+        if (qtde < 3) {
+            valor = 140 * qtde
+        } else {
+            valor = 130 * qtde
+        }
+    } else {
+        valor = 110 * qtde
+    }
+    document.getElementById("total").value = valor
+}
